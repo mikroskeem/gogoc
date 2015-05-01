a28f51756080f6d1df4b0057106ba3cba9eb6ae4
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2015-05-01 11:34:07 +0300

    Fix 'src/ipcserver.cc:133:15: warning: using the result of an assignment as a condition without parentheses [-Wparentheses]'

--- gogoc-messaging/src/ipcserver.cc
+++ gogoc-messaging/src/ipcserver.cc
@@ -130,7 +130,7 @@ bool IPCServer::WaitReady( unsigned long ulWaitms )
 {
   bool bRetVal;
 
-  if( bRetVal = (m_pSemReadyState->WaitAndLock( ulWaitms ) == 0) )
+  if( (bRetVal = (m_pSemReadyState->WaitAndLock( ulWaitms ) == 0)) )
     m_pSemReadyState->ReleaseLock();
 
   return bRetVal;
